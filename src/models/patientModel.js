bf17f94c4e28d93ab94907d24c0e36325915ccec
Shikhar Mall <shmall.21.2020@gmail.com>
2024-05-24 20:47:15 +0530

    small bug solved

--- src/models/patientModel.js
+++ src/models/patientModel.js
@@ -13,7 +13,7 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [10, 255], // Minimum 10 characters
+        len: [10, 255],
       },
     },
     email: {
@@ -27,14 +27,14 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [10, 20], // Minimum 10 characters (including country code)
+        len: [10, 20],
       },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [8, 15], // Minimum 8, Maximum 15 characters
+        len: [8, 15], 
         isStrongPassword(value) {
           if (!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/.test(value)) {
             throw new Error(
